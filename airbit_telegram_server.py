b7fe75cfbe72975f56f15c768936d1145567beba
luke <chargerunit@gmail.com>
2017-11-04 16:27:19 +0900

    .

--- airbit_telegram_server.py
+++ airbit_telegram_server.py
@@ -1,7 +1,21 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
+import telegram
 import os
 import random
+
+# 봇이 응답할 명령어
+CMD_START     = '/start'
+CMD_STOP      = '/stop'
+CMD_HELP      = '/help'
+CMD_BROADCAST = '/broadcast'
+
+# 커스텀 키보드
+CUSTOM_KEYBOARD = [
+        [CMD_START],
+        [CMD_STOP],
+        [CMD_HELP],
+        ]
 
 # 로그 처리
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -16,11 +30,23 @@ def iam(bot, update):
     msg = "my chat_id is %d" % (chat_id)
     bot.sendMessage(update.message.chat_id, text=msg)
 
+def remove_custom_keyboard(bot, update):
+    reply_markup = telegram.ReplyKeyboardRemove()
+    bot.send_message(update.message.chat_id, text="I'm back.", reply_markup=reply_markup)
+
 def start(bot, update):
-    chat_id = update.message.chat_id
+
+    kb = [[telegram.KeyboardButton('command1')],
+          [telegram.KeyboardButton('/command2')],
+          [telegram.KeyboardButton('command3')],
+          [telegram.KeyboardButton('/command4')],
+          [telegram.KeyboardButton('/command5')]]
+    kb_markup = telegram.ReplyKeyboardMarkup(kb)
+
     user = update.message.from_user
     msg = "안녕하세요, %s %s! 에어비트클럽 공지방에 오신걸 환영 합니다." %(user.first_name, user.last_name)
-    bot.sendMessage(update.message.chat_id, text=msg)
+    bot.sendMessage(update.message.chat_id, text=msg, reply_markup=kb_markup)
+
 
 def query(msg):
     return msg
@@ -34,7 +60,7 @@ def response(bot, update):
     bot.sendMessage(chat_id, text=r_msg)
     bot.sendMessage(chat_id, text=user_name)
 
-def error(bot, update, error):
+def error(update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
 
 def main():
